ff3360dabca7a1958f75da90dda3cdde5d24c4c1
Merryn <merrynhr@gmail.com>
2021-08-03 13:51:38 +0100

    money format with two decimal places

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,22 +3,34 @@ require_relative 'bank'
 class Statement 
 
 
-   def initialize (date_array, credit_array, debit_array, balance_array)
-      @date_array = date_array
-      @credit_array = credit_array
-      @debit_array = debit_array
-      @balance_array = balance_array
-      @counter = -1
-      
-  end
+  def initialize (date_array, credit_array, debit_array, balance_array)
+     @date_array = date_array
+     @credit_array = credit_array
+     @debit_array = debit_array
+     @balance_array = balance_array
+     @i = -1
+     
+ end
 
+ def money_format
+   make_demcimals
+ end
 
-  def print_statement 
-    puts "date || credit || debit || balance" 
-    while @counter > -1 - @balance_array.length
-      puts "#{@date_array[@counter]} || #{@credit_array[@counter]} || #{@debit_array[@counter]} || #{@balance_array[@counter]}"
-      #puts "#{Date.today} || #{sprintf "%.2f", @balance} "
-      @counter -=1
-    end
-  end
+ def print_statement 
+   puts "date || credit || debit || balance" 
+   money_format
+   while @i > -1 - @balance_array.length
+     puts "#{@date_array[@i]} || #{@credit_array[@i]} || #{@debit_array[@i]} || #{@balance_array[@i]}"
+     @i -=1
+   end
+ end
+ 
+ private 
+
+ def make_demcimals
+   @balance_array.map! { |b| sprintf "%.2f", b }
+   @credit_array.map! { |c| sprintf "%.2f", c }
+   @debit_array.map! { |d| sprintf "%.2f", d }
+ end
+
 end
