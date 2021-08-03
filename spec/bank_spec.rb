a7a63ee70d770f5a5add09853469739f9fbc3724
Merryn <merrynhr@gmail.com>
2021-08-03 15:06:21 +0100

    before block added to bank_spec

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,34 +2,34 @@ require './lib/bank.rb'
 require 'date'
 
 describe Bank do
+
+  before(:each) do 
+    @bank = Bank.new
+  end
+
   it 'has zero money before transactions have taken place' do
-    bank = Bank.new
-    expect(bank.balance).to eq(0)
+    expect(@bank.balance).to eq(0)
   end
 
   it 'can deposit money' do
-    bank = Bank.new
-    bank.deposit(1000)
-    expect(bank.balance).to eq(1000)
+    @bank.deposit(1000)
+    expect(@bank.balance).to eq(1000)
   end
 
   it 'can withdraw money' do
-    bank = Bank.new
-    bank.deposit(1000)
-    bank.withdrawal(500)
-    expect(bank.balance).to eq(500)
+    @bank.deposit(1000)
+    @bank.withdrawal(500)
+    expect(@bank.balance).to eq(500)
   end
 
   it 'can go into overdraft' do
-    bank = Bank.new
-    bank.withdrawal(500)
-    expect(bank.balance).to eq(-500)
+    @bank.withdrawal(500)
+    expect(@bank.balance).to eq(-500)
   end
 
   it 'can save the date of transactions' do
-    bank = Bank.new
-    bank.deposit(1000)
-    expect(bank.date_array[0]).to eq(Date.today)
+    @bank.deposit(1000)
+    expect(@bank.date_array[0]).to eq(Date.today)
   end
 
 end
